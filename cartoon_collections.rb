922db9568efd27be6adacde500e8b447a5afc8ad
Laura <lnkeat@gmail.com>
2021-11-11 12:33:10 -0700

    commit tree 8713e7f96cb560e5873988ed1b6f5d37112b643f

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,25 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+require 'pry'
+
+def roll_call_dwarves(arr)
+  arr.map.with_index { |dwarf, i| puts "#{i + 1}. #{dwarf}"}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(arr)
+  return arr.map{ |ele| "#{ele[0].upcase + ele.slice(1..-1)}!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(arr)
+  return arr.find { |ele| ele.length > 4 } == nil ? false : true
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(arr)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  arr.each { |ele| return ele if cheese_types.include?(ele)}
+  nil
 end
+
+dwarves = %w[Dopey Grumpy Bashful]
+calls = %w[earth wind fire water heart]
+calls_long = ["axe", "earth", "wind", "fire"]
+cheddar_cheese = %w[banana cheddar sock]
+
